28fd23e2d64ef75c3c7fe9230458317fcc2b0a77
damisotomi <sotomidamilola@gmail.com>
2022-09-17 20:20:12 +0100

    Added site link to allowed host

--- news/settings.py
+++ news/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@dn2a)mj%zkkay%&%_)aveg99zmxbqzhh7jq+*5=gpo0v7#!-_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://sotomi-navigatenews.herokuapp.com/','http://127.0.0.1:8000/','127.0.0.1:8000/']
+ALLOWED_HOSTS = ['https://sotomi-navigatenews.herokuapp.com/','sotomi-navigatenews.herokuapp.com','http://127.0.0.1:8000/','127.0.0.1:8000/']
 
 
 # Application definition
